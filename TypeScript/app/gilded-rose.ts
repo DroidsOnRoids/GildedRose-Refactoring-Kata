a2056b6090c5d622488049fb076f34a7ce662f5e
Bartek <bartek@MacBook-Air-Bartek.local>
2018-09-05 20:17:42 +0200

    Clean up main function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,22 +10,7 @@ export class GildedRose {
 
     updateQuality() {
         for (let i=0; i < this.items.length; i+=1) {
-            const rawItem = this.items[i];
-            let item = ItemFactory.getItem(rawItem);
-            // switch (rawItem.name) {
-            //     case GildedRose.AGED_BRIE:
-            //         item = new AgedBrie(rawItem.name, rawItem.sellIn, rawItem.quality);
-            //         break;
-            //     case GildedRose.BACKSTAGE:
-            //         item = new Backstage(rawItem.name, rawItem.sellIn, rawItem.quality);
-            //         break;
-            //     case GildedRose.SULFURAS:
-            //         item = new Sulfuras(rawItem.name, rawItem.sellIn, rawItem.quality);
-            //         break;
-            //     default:
-            //         item = new RegularItem(rawItem.name, rawItem.sellIn, rawItem.quality);
-            //         break;
-            // }
+            let item = ItemFactory.getItem(this.items[i]);
             item.update();
             this.items[i] = item;
         }
